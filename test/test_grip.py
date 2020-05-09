ab353a887c6fd52b51a83d334e773336b21b21c6
Gavin Stark <gjstark@me.com>
2020-05-09 13:24:20 +0100

    [test] Tidying git repository handling

--- test/test_grip.py
+++ test/test_grip.py
@@ -15,7 +15,7 @@ from .test_lib.loggable import TestLog, Loggable
 from .test_lib.os_command import OSCommand
 
 from .test_lib.git import RepoBuildContentFn
-from .test_lib.git import Repository as GitRepoBuild
+from .test_lib.git import Repository as GitRepository
 
 from typing import List, Callable, Optional, Any, ClassVar, cast
 
@@ -26,7 +26,7 @@ sys.path.append(grip_dir)
 
 #a Test classes
 #c Grip repo building class
-class GripRepoBuild(GitRepoBuild):
+class GripRepoBuild(GitRepository):
     grip_toml = """
     name = "test_grip"
     default_config  = "cfg0"
@@ -45,14 +45,20 @@ class GripRepoBuild(GitRepoBuild):
     #f __init__
     def __init__(self, name:str, fs:FileSystem, log:TestLog, **kwargs:Any) -> None:
         global grip_dir
-        GitRepoBuild.__init__(self, name=name, fs=fs, log=log, init_content=self.init_content, **kwargs)
+        GitRepository.__init__(self, name=name, fs=fs, log=log, **kwargs)
         self.grip_exec = os.path.join(grip_dir,"grip")
         pass
+    #f git_init
+    def git_init(self, init_content:Optional[RepoBuildContentFn]=None) -> 'GripRepoBuild':
+        GitRepository.git_init(self, init_content = self.init_content)
+        return self
     #f init_content
-    def init_content(self, git_repo:GitRepoBuild) -> None:
-        git_repo.make_dir([".grip"])
-        git_repo.create_file(paths=[".grip","grip.toml"], content=FileContent(self.grip_toml.format(fs_path=self.fs.path)))
-        git_repo.git_command(wd=".grip", cmd="add grip.toml")
+    @staticmethod
+    def init_content(repo:'GitRepository') -> None:
+        self = cast(GripRepoBuild,repo)
+        self.make_dir([".grip"])
+        self.create_file(paths=[".grip","grip.toml"], content=FileContent(self.grip_toml.format(fs_path=self.fs.path)))
+        self.git_command(wd=".grip", cmd="add grip.toml")
         pass
     #f grip_command
     def grip_command(self, cmd:str, wd:Optional[str]=None, **kwargs:Any) -> str:
@@ -69,23 +75,23 @@ class GripRepoBuild(GitRepoBuild):
 class Test(unittest.TestCase):
     #v class properties
     cls_fs      : ClassVar[FileSystem]
-    cls_d1      : ClassVar[GitRepoBuild]
-    cls_d1_bare : ClassVar[GitRepoBuild]
-    cls_d2      : ClassVar[GitRepoBuild]
-    cls_d2_bare : ClassVar[GitRepoBuild]
+    cls_d1      : ClassVar[GitRepository]
+    cls_d1_bare : ClassVar[GitRepository]
+    cls_d2      : ClassVar[GitRepository]
+    cls_d2_bare : ClassVar[GitRepository]
     cls_g       : ClassVar[GripRepoBuild]
-    cls_g_bare  : ClassVar[GitRepoBuild]
+    cls_g_bare  : ClassVar[GitRepository]
     #f setUpClass - invoked for all tests to use
     @classmethod
     def setUpClass(cls) -> None:
         try:
             fs = FileSystem()
             cls.cls_fs = fs
-            cls.cls_d1       = GitRepoBuild(name="d_1",fs=fs,parent_dirs=[],log=test_logger)
+            cls.cls_d1       = GitRepository(name="d_1",fs=fs,parent_dirs=[],log=test_logger).git_init(GitRepository.add_readme)
             cls.cls_d1_bare  = cls.cls_d1.bare_clone()
-            cls.cls_d2       = GitRepoBuild(name="d_2",fs=fs,parent_dirs=[],log=test_logger)
+            cls.cls_d2       = GitRepository(name="d_2",fs=fs,parent_dirs=[],log=test_logger).git_init(GitRepository.add_readme)
             cls.cls_d2_bare  = cls.cls_d2.bare_clone()
-            cls.cls_g        = GripRepoBuild(name="grip_1",fs=fs,log=test_logger)
+            cls.cls_g        = GripRepoBuild(name="grip_1",fs=fs,log=test_logger).git_init()
             cls.cls_g_bare   = cls.cls_g.bare_clone()
         except:
             test_logger.tidy()
@@ -115,7 +121,7 @@ class Test(unittest.TestCase):
     #f test_git_clone
     def test_git_clone(self) -> None:
         fs = FileSystem()
-        d2 = GitRepoBuild(name="grip_repo_one_clone",fs=fs,clone=self.cls_d1_bare.path,log=test_logger)
+        d2 = GitRepository(name="grip_repo_one_clone",fs=fs,log=test_logger).git_clone(clone=self.cls_d1_bare.path, bare=False)
         d2.append_to_file(["Readme.txt"], content=FileContent("Appended text"))
         d2.git_command("commit -a -m 'appended text to readme'")
         d2.git_command_allow_stderr("push", )
@@ -124,7 +130,8 @@ class Test(unittest.TestCase):
     #f test_grip_interrogate
     def test_grip_interrogate(self) -> None:
         fs = FileSystem()
-        g = GripRepoBuild(name="grip_repo_one_clone",fs=fs,log=test_logger,clone=self.cls_g_bare.path)
+        g = GripRepoBuild(name="grip_repo_one_clone",fs=fs,log=test_logger)
+        g.git_clone(clone=self.cls_g_bare.path)
         g.grip_command("configure")
         grip_root = g.grip_command("root")
         checkout_path = os.path.realpath(g.path)
@@ -142,7 +149,8 @@ class Test(unittest.TestCase):
     #f test_grip_configure
     def test_grip_configure(self) -> None:
         fs = FileSystem() # "test_configure")
-        g = GripRepoBuild(name="grip_repo_one_clone",fs=fs,clone=self.cls_g_bare.path,log=test_logger)
+        g = GripRepoBuild(name="grip_repo_one_clone",fs=fs,log=test_logger)
+        g.git_clone(clone=self.cls_g_bare.path)
         g.grip_command("configure")
         #print(os_command(options=g.options, cmd="cat .grip/grip.toml", cwd=g.path))
         self.assertTrue(os.path.isdir(fs.abspath(["grip_repo_one_clone"])), "git clone of bare grip repo should create grip_repo_one_clone directory")
--- test/test_lib/git.py
+++ test/test_lib/git.py
@@ -22,37 +22,47 @@ class Repository(Loggable):
     readme_text = """
     This is a simple test git repo
     """
-    #f __init__
-    def __init__(self, name:str, fs:FileSystem, log:TestLog, parent_dirs:List[str]=[], clone:Optional[str]=None, bare:bool=False, init_content:Optional[RepoBuildContentFn]=None, branch_name:str="master") -> None:
+    git_repo : GitRepo
+    #f __init__ - must do a git_init or git_clone afterwards
+    def __init__(self, name:str, fs:FileSystem, log:TestLog, parent_dirs:List[str]=[], **kwargs:Any):
         Loggable.__init__(self, log)
         self.name = name
         self.path = fs.abspath(parent_dirs+[name])
         self.fs = fs
         self.options = Options()
         self.options.verbose = False
-        if clone is None:
-            self.fs.make_dir([self.name])
-            self.git_command(cmd="init")
-            if init_content is not None:
-                init_content(self)
-                pass
-            else:
-                self.fs.create_file(paths=[self.name,"Readme.txt"], content=FileContent(self.readme_text))
-                self.git_command(cmd="add Readme.txt")
-                pass
-            self.git_command(cmd="commit -m Init -a")
-            self.git_repo = GitRepo(log=self.logger(), path_str=self.path, permit_no_remote=True)
+        pass
+    #f git_clone
+    def git_clone(self, clone:str, bare:bool=False, branch_name:str="master") -> 'Repository':
+        self.git_repo = GitRepo.clone(log=self.logger(), options=self.options, repo_url=clone, dest=self.path, bare=bare, new_branch_name=branch_name)
+        if not bare:
+            upstream =self.git_repo.get_upstream()
+            if upstream is not None: self.git_repo.set_upstream_of_branch(branch_name, upstream)
+            pass
+        return self
+    #f git_init - do a git init, add a readme, and commit
+    def git_init(self, init_content:Optional[RepoBuildContentFn]=None) -> 'Repository':
+        self.fs.make_dir([self.name])
+        self.git_command(cmd="init")
+        if init_content is not None: init_content(self)
+        self.git_command(cmd="commit -m Init -a")
+        self.git_repo = GitRepo(log=self.logger(), path_str=self.path, permit_no_remote=True)
+        return self
+    #f add_readme
+    @staticmethod
+    def add_readme(self:'Repository', content:Optional[FileContent]=None) -> None:
+        if content is None:
+            file_content = FileContent(self.readme_text)
             pass
         else:
-            self.git_repo = GitRepo.clone(log=self.logger(), options=self.options, repo_url=clone, dest=self.path, bare=bare, new_branch_name=branch_name)
-            if not bare:
-                upstream =self.git_repo.get_upstream()
-                if upstream is not None: self.git_repo.set_upstream_of_branch(branch_name, upstream)
+            file_content = content
             pass
+        self.fs.create_file(paths=[self.name,"Readme.txt"], content=file_content)
+        self.git_command(cmd="add Readme.txt")
         pass
     #f bare_clone
     def bare_clone(self) -> 'Repository':
-        return  self.__class__(name="%s.git"%(self.name),fs=self.fs, log=self.logger(), clone=self.path, bare=True)
+        return  self.__class__(name="%s.git"%(self.name), fs=self.fs, log=self.logger()).git_clone(clone=self.path, bare=True)
     #f make_dir
     def make_dir(self, paths:PathList) -> None:
         self.add_log_string("Making directory %s"%str([self.name]+paths))
