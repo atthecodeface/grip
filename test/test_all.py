63eb7202b69ebe75c571aad217234f3f5a0760e7
Gavin Stark <gjstark@me.com>
2020-05-11 11:13:45 +0100

    [test] WIP

--- test/test_all.py
+++ test/test_all.py
@@ -5,10 +5,8 @@ Unittest harness to run test cases from lib directory
 
 #a Imports
 import os, unittest
-# from .test_grip_desc import test_suite as grip_desc_test_suite
 import importlib
 
-log_dir       = os.environ["TESTS_LOG_DIR"]
 #from .test_grip  import *
 from .test_git   import *
 #from .test_state_file  import *
@@ -19,9 +17,8 @@ def add_test_suite(module_name:str)->None:
     if not hasattr(m,"test_suite"):
         raise Exception("Failed to import test_suite from %s - it did not have at test_suite attribute"%module_name)
     for t in m.test_suite:
-        if hasattr(m,t):
-            globals()[t]=getattr(m,t)
-            pass
+        print(t.__name__)
+        # globals()[t.__name__]=t
         pass
     pass
 add_test_suite(".test_grip_desc")
